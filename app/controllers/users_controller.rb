d7e600f52ebab1802b3fa38377e95998412b6752
Charles Zhang <x.charles.z@gmail.com>
2011-03-30 00:02:54 -0500

    done with user edit/update, index, and others

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index,:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+  
   def new
     @user = User.new
     @title = "Sign Up"
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
   
   def show
@@ -21,7 +30,40 @@ class UsersController < ApplicationController
     end
   end
   
-
-
+  def edit
+    @title = "Edit User"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+  
 end
 
